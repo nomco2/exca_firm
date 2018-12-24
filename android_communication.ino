bd58d77fc46735039ce96ad65dfaaf6ace30b61a
nomco2 <nomco2@naver.com>
2018-12-24 22:17:39 +0900

    auto laser off

--- android_communication.ino
+++ android_communication.ino
@@ -1,191 +1,206 @@
 #include "eeprom.h"
 
 
-void cos_amend_value_init(){
-    cos_amend_value = (float)EEPROMRead_int(1)/(float)100;
+
+void cos_amend_value_init() {
+  cos_amend_value = (float)EEPROMRead_int(1) / (float)100;
 }
 
 
-void receive_laser_data(){
+void receive_laser_data() {
   laser_35_dollars_loop();
-  height_calculation = int(cos(abs(kalAngleY)*cos_amend_value) * laser_data_int);
+  height_calculation = int(cos(abs(kalAngleY) * cos_amend_value) * laser_data_int);
 }
 
 
-
-void serial_read_processing_to_divided_mode(char read_data){
+unsigned long Motor_control_Millis = 0; //시간이 1분 지나면 자동으로 레이저 종료
+void serial_read_processing_to_divided_mode(char read_data) {
   String json_name = "amend_value";
   String json_int;
-  switch(read_data){
+  switch (read_data) {
     case 'd':
       continue_or_not = true;
       laser_35_dollars.write('D');
-    break;
+      Motor_control_Millis = millis();
+      break;
     case 'x':
       continue_or_not = false;
-    break;
-    
+      break;
+
     case 'o':
       laser_35_dollars.write('O');
-    break;
+      break;
     case 'c':
       laser_35_dollars.write('C');
-    break;
+      break;
     case 's':
 
-    break;
+      break;
 
     /* change amend value */
-    case 'E': 
+    case 'E':
 
-        char data[3];
-        Serial.readBytes(data, sizeof(data));
-        EEPROMWrite_int(1,atoi(data));
-        delay(100);
-        cos_amend_value = (float)EEPROMRead_int(1)/(float)100;
-//        Serial.println(cos_amend_value);
-
-        
-        json_int = String((float)atoi(data)/(float)100);
-        Serial.println(json_maker(&json_name, &json_int));
+      char data[3];
+      Serial.readBytes(data, sizeof(data));
+      EEPROMWrite_int(1, atoi(data));
+      delay(100);
+      cos_amend_value = (float)EEPROMRead_int(1) / (float)100;
+      //        Serial.println(cos_amend_value);
 
 
-    break;
+      json_int = String((float)atoi(data) / (float)100);
+      Serial.println(json_maker(&json_name, &json_int));
 
-    case 'R': 
-        cos_amend_value = (float)EEPROMRead_int(1)/(float)100;
-        
-        json_int = String(cos_amend_value);
-        Serial.println(json_maker(&json_name, &json_int));
 
-    break;
-    
+      break;
+
+    case 'R':
+      cos_amend_value = (float)EEPROMRead_int(1) / (float)100;
+
+      json_int = String(cos_amend_value);
+      Serial.println(json_maker(&json_name, &json_int));
+
+      break;
+
 
     /*motor control */
     case 'm':
+      Motor_control_Millis = millis();
       char motor_direction = Serial.read();
       int ms = 180; //motor speed
-      switch(motor_direction){
+      switch (motor_direction) {
         case '0':
           digitalWrite(2, true);
           digitalWrite(4, true);
           digitalWrite(5, true);
           digitalWrite(7, true);
-//          Serial.print("0");
+          //          Serial.print("0");
           break;
-          
+
         case '1': //left_up
-          Motor_operation(ms,false,ms,false);
+          Motor_operation(ms, false, ms, false);
           break;
         case '2': //middle_up
-          Motor_operation(ms,false,0,true);
+          Motor_operation(ms, false, 0, true);
           break;
         case '3'://right_up
-          Motor_operation(ms,false,ms,true);
+          Motor_operation(ms, false, ms, true);
           break;
         case '4'://left
-          Motor_operation(0,true,ms,false);
+          Motor_operation(0, true, ms, false);
           break;
 
         case '6'://right
-          Motor_operation(0,true,ms,true);
+          Motor_operation(0, true, ms, true);
           break;
         case '7'://left_down
-          Motor_operation(ms,true,ms,false);
+          Motor_operation(ms, true, ms, false);
           break;
         case '8'://middle_down
-          Motor_operation(ms,true,0,true);
+          Motor_operation(ms, true, 0, true);
           break;
         case '9'://right_down
-          Motor_operation(ms,true,ms,true);
+          Motor_operation(ms, true, ms, true);
           break;
       }
       break;
 
 
-      
-    
-    
+
+
+
   }
 }
 
 
 
 
-void D_processing(){
-    char data = laser_35_dollars.read();
-    boolean D_send_once = true;
-//    Serial.print(data);
-    if(data == ' '){
-      D = true;
-    }else if(data == 'm'){
-      D = false;
-      laser_data_int = data_string.toInt();
-      data_string = "";
-//      String Quotes = "'";
-//      Serial.print("[{");
-//      Serial.print(Quotes);
-//      Serial.print("distance");
-//      Serial.print(Quotes);
-//      Serial.print(":");
-//      Serial.print(laser_data_int);
-//
-//      Serial.print(",");
-//      Serial.print(Quotes);
-//      Serial.print("angle");
-//      Serial.print(Quotes);
-//      Serial.print(":");
-//      Serial.print(kalAngleY);
-//
-//      Serial.print(",");
-//      Serial.print(Quotes);
-//      Serial.print("height");
-//      Serial.print(Quotes);
-//      Serial.print(":");
-//      Serial.println(height_calculation);
-//      Serial.print("}]");
+void D_processing() {
+  char data = laser_35_dollars.read();
+  boolean D_send_once = true;
+  //    Serial.print(data);
+  if (data == ' ') {
+    D = true;
+  } else if (data == 'm') {
+    D = false;
+    laser_data_int = data_string.toInt();
+    data_string = "";
+    //      String Quotes = "'";
+    //      Serial.print("[{");
+    //      Serial.print(Quotes);
+    //      Serial.print("distance");
+    //      Serial.print(Quotes);
+    //      Serial.print(":");
+    //      Serial.print(laser_data_int);
+    //
+    //      Serial.print(",");
+    //      Serial.print(Quotes);
+    //      Serial.print("angle");
+    //      Serial.print(Quotes);
+    //      Serial.print(":");
+    //      Serial.print(kalAngleY);
+    //
+    //      Serial.print(",");
+    //      Serial.print(Quotes);
+    //      Serial.print("height");
+    //      Serial.print(Quotes);
+    //      Serial.print(":");
+    //      Serial.println(height_calculation);
+    //      Serial.print("}]");
 
-      String json_name[4] = {"distance", "angle1", "angle2", "height"};
-      String json_int[4];
-      json_int[0] = String(laser_data_int);
-      json_int[1] = String(kalAngleX);
-      json_int[2] = String(kalAngleY);
-      json_int[3] = String(height_calculation);
-      Serial.println(json_maker(json_name, json_int));
-    }    
-    if(D && data != '.'){
-      data_string += data;
-      D_send_once = true;
-    }else{
-      if(D_send_once){
-        D_send_once = false;
-        
-      }
+    String json_name[4] = {"distance", "angle1", "angle2", "height"};
+    String json_int[4];
+    json_int[0] = String(laser_data_int);
+    json_int[1] = String(kalAngleX);
+    json_int[2] = String(kalAngleY);
+    json_int[3] = String(height_calculation);
+    Serial.println(json_maker(json_name, json_int));
+  }
+  if (D && data != '.') {
+    data_string += data;
+    D_send_once = true;
+  } else {
+    if (D_send_once) {
+      D_send_once = false;
+
     }
-    
+  }
+
 
 }
 
 
 
-void laser_35_dollars_loop(){
+void laser_35_dollars_loop() {
   if (laser_35_dollars.available()) {
-   
+
     D_processing();  //result is saved the data_int
-    
-  }else{
-        if(continue_or_not){
-          delay(100);
-          laser_35_dollars.write('D');
-//          Serial.println("d send");
-        }
+
+  } else {
+    if (continue_or_not) {
+      delay(100);
+      laser_35_dollars.write('D');
+      //          Serial.println("d send");
+    }
   }
-  
+
   if (Serial.available()) {
-//    laser_35_dollars.write(Serial.read());
-      char a = Serial.read();
-//      Serial.print(a);
-      serial_read_processing_to_divided_mode(a);
+    //    laser_35_dollars.write(Serial.read());
+    char a = Serial.read();
+    //      Serial.print(a);
+    serial_read_processing_to_divided_mode(a);
+  }
+
+  if ((millis() - Motor_control_Millis) > 300000 && continue_or_not) {
+    continue_or_not = false;
+    
+    String json_name[4] = {"distance", "angle1", "angle2", "height"};
+    String json_int[4];
+    json_int[0] = "-1";
+    json_int[1] = "0";
+    json_int[2] = "0";
+    json_int[3] = "0";
+    Serial.println(json_maker(json_name, json_int));
   }
 
 
