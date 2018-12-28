cf5021e38aa24c293edd4adc8997550543afc040
nomco2 <nomco2@naver.com>
2018-12-28 11:08:03 +0900

    1

--- eeprom.h
+++ eeprom.h
@@ -13,6 +13,7 @@ void EEPROMWrite_int(int address, int value)
       byte two = ((value >> 16) & 0xFF);
       byte one = ((value >> 24) & 0xFF);
 
+
       //Write the 4 bytes into the eeprom memory.
       EEPROM.write(address, four);
       EEPROM.write(address + 1, three);
@@ -25,10 +26,10 @@ void EEPROMWrite_int(int address, int value)
 int EEPROMRead_int(int address)
       {
       //Read the 4 bytes from the eeprom memory.
-      int four = EEPROM.read(address);
-      int three = EEPROM.read(address + 1);
-      int two = EEPROM.read(address + 2);
-      int one = EEPROM.read(address + 3);
+      long int four = EEPROM.read(address);
+      long int three = EEPROM.read(address + 1);
+      long int two = EEPROM.read(address + 2);
+      long int one = EEPROM.read(address + 3);
 
       //Return the recomposed long by using bitshift.
       return ((four << 0) & 0xFF) + ((three << 8) & 0xFFFF) + ((two << 16) & 0xFFFFFF) + ((one << 24) & 0xFFFFFFFF);
